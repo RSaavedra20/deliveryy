5b7b62db7eb1d0086af4f40f1f991cff4eefa0c4
vbarraurzua <v.barra@duocuc.cl>
2022-10-18 14:42:45 -0300

    Actualización code

--- delivery/src/app/combo2/combo2.page.ts
+++ delivery/src/app/combo2/combo2.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { Animation, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-combo2',
@@ -7,9 +9,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Combo2Page implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private animationCtrl: AnimationController) { 
+    
+  }
+    
 
   ngOnInit() {
   }
+  user={
+    usuario:"",
+    password:""
+  }
+  ingresar(){
+    const animation: Animation = this.animationCtrl.create()
+      .addElement(document.querySelector('.animacion'))
+      .duration(1000)
+      .fromTo('opacity', '1', '0.2')
+      .duration(1500)
+      .iterations(Infinity)
+      .fromTo('transform', 'translateX(0px)', 'translateX(100px)')
+      .fromTo('opacity', '1', '0.2');
+    animation.play();
+
+    
+    // Se declara e instancia un elemento de tipo NavigationExtras
+    let navigationExtras: NavigationExtras = {
+      state: {
+        user: this.user // Al estado se asignamos un objeto con clave y valor
+      }
+    };
+    this.router.navigate(['/pagina-inicial'],navigationExtras); // navegamos hacia el Home y enviamos información adicional
+  }
+
+  tuPromo(){
+    // Se declara e instancia un elemento de tipo NavigationExtras
+    let navigationExtras: NavigationExtras = {
+      state: {
+        user: this.user // Al estado se asignamos un objeto con clave y valor
+      }
+    };
+    this.router.navigate(['/combo2'],navigationExtras); // navegamos hacia el Home y enviamos información adicional
+  }
+
+  back(){
+    // Se declara e instancia un elemento de tipo NavigationExtras
+    let navigationExtras: NavigationExtras = {
+      state: {
+        user: this.user // Al estado se asignamos un objeto con clave y valor
+      }
+    };
+    this.router.navigate(['/login'],navigationExtras); // navegamos hacia el Home y enviamos información adicional
+  }
+
+  
+
+
+
+
 
 }
+
+
+
